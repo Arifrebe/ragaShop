91b92e1b8ebe839bd35056f9b9a3254367eed8c8
rebe <arifharahap057@gmail.com>
2026-04-02 02:57:31 +0700

    add midtrans

--- routes/front/route.php
+++ routes/front/route.php
@@ -14,4 +14,7 @@ Route::name('front.')->group(function () {
     Route::get('/checkout', [ShopController::class, 'checkout'])->name('checkout');
     Route::get('/about', [AboutController::class, 'index'])->name('about');
     Route::get('/contact', [ContactUsController::class, 'index'])->name('contact');
+
+    Route::post('/process-checkout', [ShopController::class, 'processCheckout']);
+    Route::post('/callback', [ShopController::class, 'callback']);
 });
